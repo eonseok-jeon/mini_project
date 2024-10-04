4a45ff4f2db795b2abea3b052f86b4a48f9016f5
Jeon Eonseok <jseokeon@gmail.com>
2024-10-04 15:40:02 +0900

    chore: 보여지는 카드 20개로 수정

--- app/_views/Contents/index.tsx
+++ app/_views/Contents/index.tsx
@@ -13,7 +13,7 @@ export default function Contents() {
       '%7B%22%24and%22%3A%5B%7B%22title%22%3A%22%25%25%22%7D%2C%7B%22%24or%22%3A%5B%7B%22status%22%3A2%7D%2C%7B%22status%22%3A3%7D%2C%7B%22status%22%3A4%7D%5D%7D%2C%7B%22%24or%22%3A%5B%5D%7D%2C%7B%22is_datetime_enrollable%22%3Atrue%7D%5D%7D',
     sort_by: 'created_datetime.desc',
     offset: '0',
-    count: '12',
+    count: '20',
   });
 
   const { data } = useQuery({
